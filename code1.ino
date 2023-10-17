3559efe86d035e0ea5a2dc7899eac296d1a42cce
Mike Brandon <55140732+MikeBrandon@users.noreply.github.com>
2023-10-17 21:13:09 +0300

    Update code1.ino

--- code1.ino
+++ code1.ino
@@ -1,207 +1,146 @@
 #include <SPI.h> 
-
 #include <MFRC522.h>
-
 #include <Wire.h> 
-
 #include <LiquidCrystal_I2C.h>
-
-
+#include <SoftwareSerial.h>
 
 #define SS_PIN 10 
-
 #define RST_PIN 9 
-
-
-
 #define LED_DENIED_PIN 7
-
 #define LED_ACCESS_PIN 6
 
+//Create software serial object to communicate with SIM800L
+SoftwareSerial mySerial(3, 2); //SIM800L Tx & Rx is connected to Arduino #3 & #2
 
-
-LiquidCrystal_I2C lcd(0x3F,16,2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Instance of the class
 
-
-
-int code[] = {32,154,149,117}; //This is the stored UID (Unlock Card)
+#define numberOfPass 10
+String authorisedCode[numberOfPass] = { "3574122247", "1952110254" };
+bool userStatus[numberOfPass] = { false, false };
 
 int codeRead = 0;
+int userStatusCount = 0;
 
 String uidString;
 
-
-
 void setup() {
+  Serial.begin(9600);
+  //Begin serial communication with Arduino and SIM800L
+  mySerial.begin(9600);
 
-   Serial.begin(9600); 
+  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+  updateSerial();
+  mySerial.println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
+  updateSerial();
+  mySerial.println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
+  updateSerial();
+  mySerial.println("AT+CREG?"); //Check whether it has registered in the network
+  updateSerial();
 
-   SPI.begin();       // Init SPI bus
+  SPI.begin();       // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522 
 
-   mfrc522.PCD_Init(); // Init MFRC522 
+  Serial.println("Arduino RFID reading UID");
+  pinMode( LED_DENIED_PIN , OUTPUT); 
+  pinMode( LED_ACCESS_PIN , OUTPUT);
 
-   Serial.println("Arduino RFID reading UID");
-
-
-
-   pinMode( LED_DENIED_PIN , OUTPUT); 
-
-   pinMode( LED_ACCESS_PIN , OUTPUT);
-
-
-
-   lcd.init();
-
+  lcd.init();
   // Print a message to the LCD.
-
   lcd.backlight();
-
   lcd.setCursor(0,0);
-
   lcd.print("Show your card:)");
-
 }
 
 void loop() {
+  updateSerial();
 
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+
+  lcd.clear();
+  Serial.print("Tag UID:");
+  lcd.setCursor(0,0);
+  lcd.print("Tag UID:");
+  lcd.setCursor(0,1);
+
+  String readCardId = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    readCardId.concat(mfrc522.uid.uidByte[i]);
+  }
+  Serial.print(readCardId);
+  lcd.print(readCardId);
+  Serial.println();
+
+  boolean match =  false;
+  boolean wasTrue = false;
+  for (int i = 0; i < sizeof(authorisedCode); i++) {
+    if(authorisedCode[i] == readCardId) {
+      if (userStatus[i] == true) {
+        wasTrue = true;
+      }
+      userStatus[i] = true;
+      match = true;
+    }
+  }
+
+  if (!wasTrue) {
+    userStatusCount++;
+    sendMessage("User " + readCardId + " aboard.");
+  }
+
+  delay(3000);
+
+  lcd.clear();
+  lcd.setCursor(0,0);
   
-
-if ( mfrc522.PICC_IsNewCardPresent())
-
-    {
-
-        if ( mfrc522.PICC_ReadCardSerial())
-
-        {
-
-           lcd.clear();
-
-           Serial.print("Tag UID:");
-
-           lcd.setCursor(0,0);
-
-           lcd.print("Tag UID:");
-
-           lcd.setCursor(0,1);
-
-           for (byte i = 0; i < mfrc522.uid.size; i++) {  
-
-                  Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "); 
-
-                  Serial.print(mfrc522.uid.uidByte[i], HEX);
-
-                  
-
-                  lcd.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-
-                  lcd.print(mfrc522.uid.uidByte[i], HEX);
-
-                //  lcd.print(" ");
-
-            }
-
-            
-
-            Serial.println();
-
-
-
-            int i = 0;
-
-            boolean match = true;
-
-            while(i<mfrc522.uid.size)
-
-            {
-
-    
-
-               if(!(int(mfrc522.uid.uidByte[i]) == int(code[i])))
-
-               {
-
-                  match = false;
-
-               }
-
-              i++;
-
-            }
-
-
-
-            delay(3000);
-
-           lcd.clear();
-
-           lcd.setCursor(0,0);
-
-           
-
-           
-
-           if(match)
-
-           {
-
-              digitalWrite( LED_ACCESS_PIN , HIGH);
-
-
-
-              lcd.print("Authorized access");
-
-              
-
-           
-
-    
-
-           }else{
-
-              digitalWrite( LED_DENIED_PIN , HIGH);
-
-              lcd.print(" Access denied  "); 
-
-             Serial.println("\nUnknown Card");
-
-           }
-
-             
-
-             Serial.println("============================");
-
-            
-
-             mfrc522.PICC_HaltA();
-
-
-
-             delay(3000); 
-
-             reset_state();
-
-        }
-
+  if (match) {
+    digitalWrite( LED_ACCESS_PIN , HIGH);
+    lcd.print("Authorized access");
+  } else {
+    digitalWrite( LED_DENIED_PIN , HIGH);
+    lcd.print(" Access denied  "); 
+    Serial.println("\nUnknown Card");
+  }
+  Serial.println("============================");
+  mfrc522.PICC_HaltA();
+  delay(3000); 
+  reset_state();
 }
 
+void reset_state() {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Show your card:)");
+  lcd.setCursor(0,1);
+  lcd.print("Total: ");
+  lcd.print(userStatusCount);
+  digitalWrite( LED_ACCESS_PIN , LOW);
+  digitalWrite( LED_DENIED_PIN , LOW);
 }
 
-void reset_state()
+void updateSerial() {
+  delay(500);
+  while (Serial.available()) {
+    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(mySerial.available()) {
+    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+  }
+}
 
-{
-
-    lcd.clear();
-
-    lcd.setCursor(0,0);
-
-    lcd.print("Show your card:)");
-
-    
-
-    digitalWrite( LED_ACCESS_PIN , LOW);
-
-    digitalWrite( LED_DENIED_PIN , LOW);
-
+void sendMessage(String message) {
+  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+  updateSerial();
+  mySerial.println("AT+CMGS=\"+254704351180\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+  updateSerial();
+  mySerial.print(message); //text content
+  updateSerial();
+  mySerial.write(26);
 }
